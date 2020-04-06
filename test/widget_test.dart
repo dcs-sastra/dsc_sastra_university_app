f28ad74a33a5d7a1b8b5b6ce28a5662672138b18
Seshan <zeshan.nandan@gmail.com>
2020-04-07 02:39:41 +0530

    init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dsc_sastra_university/main.dart';
+import 'package:app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
